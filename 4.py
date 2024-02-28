0e928d458dc81e5ce68b6ba67185b71aabbc9338
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2024-02-28 09:05:30 +0530

    first commit

---
+++ 4.py
@@ -0,0 +1,15 @@
+class Electronic:
+    def __init__(self):
+        print("1")
+
+class OSDevice(Electronic):
+    def __init__(self):
+        #super().__init__()
+        print("2")
+
+class Smartphone(OSDevice):
+    def __init__(self):
+        super().__init__()
+        print("3")
+
+sm=Smartphone()
